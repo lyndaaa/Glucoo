962216765c5496941409bbda4de59d76752bae94
lyndaaa <45766641+lyndaaa@users.noreply.github.com>
2018-12-10 18:14:44 +0100

    Add files via upload

---
+++ Partage.java
@@ -0,0 +1,13 @@
+package fr.lynda.sebili.mygluco;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Partage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_partage);
+    }
+}
